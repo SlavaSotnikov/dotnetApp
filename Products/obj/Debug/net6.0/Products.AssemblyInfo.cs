8d1aea59f810e0a9ef5ec14d1788f0cd0af2e27c
SlavaC <slavasotnikov2020@gmail.com>
2025-08-17 10:58:39 +0300

    upd

--- Products/obj/Debug/net6.0/Products.AssemblyInfo.cs
+++ Products/obj/Debug/net6.0/Products.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5893dea6a142d4b258ac525190127e57c8a4477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09fdf0cda9656fef2547760cbc5a872b1af09760")]
 [assembly: System.Reflection.AssemblyProductAttribute("Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
